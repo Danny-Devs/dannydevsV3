205155c6a306ed574d0b07cf9ab04ca8804d6da3
Daniel Ahn <et3rnal.d@gmail.com>
2022-07-16 02:17:58 -0400

    comment again

--- src/layouts/home.vue
+++ src/layouts/home.vue
@@ -1,12 +1,14 @@
 <template>
   <main class="h-screen relative bg-red-200">
-    <div>
-      <RouterView />
-    </div>
-
     <!-- mobile navbar disappears sm and up -->
     <div sm:hidden absolute bottom-0 bg-cyan-400 w-full>
       <MobileNavbar />
+    </div>
+
+    <!-- sm and up navbar, located at top -->
+
+    <div>
+      <RouterView />
     </div>
   </main>
 </template>
